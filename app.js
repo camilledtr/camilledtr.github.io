a76f84ab004dd00b9aa57c01b709a5cf561bdc59
camilledtr <97107181+camilledtr@users.noreply.github.com>
2022-12-29 03:08:58 +0100

    Navmenu done

--- app.js
+++ app.js
@@ -25,24 +25,26 @@ var lastScrollTop; // This Varibale will store the top position
 var navbar = document.getElementById('navbar'); // Get The NavBar
 
 window.addEventListener('scroll', function () {
-    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    //This line will get the location on scroll
+    // If the nav menu is open, we don't want the navbar to hide
+    if (!document.querySelector('.nav-menu').classList.contains('js-nav-open')) {
+        var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        //This line will get the location on scroll
 
-    if (scrollTop > lastScrollTop) { //if it is greater than the previous
-        navbar.classList.remove("scrolled-up");
-        navbar.classList.add("scrolled-down");
+        if (scrollTop > lastScrollTop) { //if it is greater than the previous
+            navbar.classList.remove("scrolled-up");
+            navbar.classList.add("scrolled-down");
+        }
+
+        else {
+            navbar.classList.remove("scrolled-down");
+            navbar.classList.add("scrolled-up");
+        }
+
+        lastScrollTop = scrollTop; //New Position Stored
     }
-
-    else {
-        navbar.classList.remove("scrolled-down");
-        navbar.classList.add("scrolled-up");
-    }
-
-    lastScrollTop = scrollTop; //New Position Stored
 });
 
 // Fix the background in the projects section ==========================================================================
-
 jQuery(document).ready(function ($) {
 
     var offset = $('.sticker').offset();
@@ -66,21 +68,68 @@ jQuery(document).ready(function ($) {
     });
 });
 
-// Open the nav menu ===================================================================================================
+// Open and close the nav menu ===================================================================================================
 var menuBtn = document.querySelector('.menu-btn');
 var BtnBar1 = document.querySelector('.menu-btn__bar-1');
 var BtnBar2 = document.querySelector('.menu-btn__bar-2');
 var logo = document.querySelector('#logo');
-
+var navCategories = document.querySelectorAll('navbar ul li');
+var darkColorPrimary = document.querySelector(':root').style.getPropertyValue('--dark-color-primary');
+var navMenuCategories = document.querySelectorAll('.nav-menu ul li');
 
 menuBtn.addEventListener('click', e => {
+    var viewportWidth = window.innerWidth;
     menuBtn.classList.toggle('js-btn-open');
     document.querySelector('.nav-menu').classList.toggle('js-nav-open');
+
     if (menuBtn.classList.contains('js-btn-open')) {
-        logo.src = "/assets/crop-light-logo.png";
-
+        BtnBar1.style.backgroundColor = "white";
+        BtnBar2.style.backgroundColor = "white";
+        setTimeout(() => {
+            logo.src = "/assets/crop-light-logo.png";
+            document.querySelector('a.logo').removeAttribute("href");
+            menuBtn.style.position = "fixed";
+            menuBtn.style.right = "3rem";
+            if (viewportWidth > 900) {
+                for (let navCategory of navCategories) {
+                    navCategory.style.display = "none";
+                }
+            }
+        }, 150);
     } else {
-        logo.src = "/assets/crop-logo.png";
-
+        BtnBar1.style.backgroundColor = darkColorPrimary;
+        BtnBar2.style.backgroundColor = darkColorPrimary;
+        setTimeout(() => {
+            menuBtn.style.position = "static";
+            menuBtn.style.right = "auto";
+            logo.src = "/assets/crop-logo.png";
+            document.querySelector('a.logo').setAttribute("href", "#home");
+            if (viewportWidth > 900) {
+                for (let navCategory of navCategories) {
+                    navCategory.style.display = "block";
+                }
+            }
+        }, 380);
     }
 });
+
+// Close the nav menu if the user clicks on a nav menu category 
+for (let navMenuCategory of navMenuCategories) {
+    navMenuCategory.addEventListener('click', e => {
+        var viewportWidth = window.innerWidth;
+        menuBtn.classList.toggle('js-btn-open');
+        document.querySelector('.nav-menu').classList.toggle('js-nav-open');
+        BtnBar1.style.backgroundColor = darkColorPrimary;
+        BtnBar2.style.backgroundColor = darkColorPrimary;
+        setTimeout(() => {
+            menuBtn.style.position = "static";
+            menuBtn.style.right = "auto";
+            logo.src = "/assets/crop-logo.png";
+            if (viewportWidth > 900) {
+                for (let navCategory of navCategories) {
+                    navCategory.style.display = "block";
+                }
+            }
+        }, 380);
+    });
+};
